14d2ebd1fdc67abc6815b9e4f3c91e8ea0718046
Patreev Igor <Patreewigor@yandex.ru>
2018-04-14 14:55:23 +0300

    Update task_performer.rb

--- app/servises/task_performer.rb
+++ app/servises/task_performer.rb
@@ -1,16 +1,20 @@
 # frozen_string_literal: true
 
+# 
+
 class TaskPerformer
   def perform_daily_tasks
-    actuall_items = item_finder.find_actuall_items
-    seller.create_orders(actuall_items)
+    актуализировать статус созданных вещей в БД
+    выставить на продажу актуализированные вещи
+    
+    seller.create_orders(item_finder.find_actuall_items)
     # users_informator.inform_user_about_sell_items
   end
 
   def perform_night_tasks
     items_editor.delete_all_items_from_trade
 
-    items_editor.actualize_item_status
+    items_editor.actualize_all_items
   end
 
   private
