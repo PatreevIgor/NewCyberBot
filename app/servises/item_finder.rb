6e78dbd549bba0ec7838095155ebc2cedde246f4
Patreev Igor <Patreewigor@yandex.ru>
2018-04-18 17:07:36 +0300

    Update item_finder.rb

--- app/servises/item_finder.rb
+++ app/servises/item_finder.rb
@@ -3,12 +3,10 @@
 class ItemFinder
   def find_actuall_items
     last_50_sales.each do |item_hash|
-      if item_validator.item_profitable?(item_hash) && item.item_not_exists?(item_hash) 
+      if item_validator.item_profitable?(item_hash) && order.order_not_exists?(item_hash) 
                                                     && item_validator.price_interval_is_valid?(item_hash)
         order.create_order(item_hash)
-        seller.put_up_purchase_order(item_hash)
       end
-      seller.create_order if item_validator.item_on_sale?(item_hash)
     end
   end
 
