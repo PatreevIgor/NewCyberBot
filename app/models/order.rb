7704e3e1bc605ea4f32aada6742dc207e061cba7
Patreev Igor <Patreewigor@yandex.ru>
2018-04-18 16:55:18 +0300

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,7 @@ class Order < ApplicationRecord
                  hash_name:   item_hash[:hash_name],
                  price:       price.price_of_buy_for_order(item_hash),
                  link:        link_generator.generate_link(item_hash),
-                 status:      Constant::NEW_ITEMS_STATUS)
+                 status:      Constant::PROFITABLE_ORDER_STATUS)
   end
 
   def actualize_orders
