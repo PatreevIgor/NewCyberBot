7185785fde9c397aee586005554e17e4d968aa95
Patreev Igor <Patreewigor@yandex.ru>
2018-04-18 14:41:50 +0300

    Update task_performer.rb

--- app/servises/task_performer.rb
+++ app/servises/task_performer.rb
@@ -2,21 +2,21 @@
 
 class TaskPerformer
   def perform_daily_tasks
-    actualize_not_actualized_items
+    actualize_not_actualized_orders
     create_buy_orders_for_profitable_orders
     item_finder.find_actuall_items
     # users_informator.inform_user_about_sell_items
   end
 
   def perform_night_tasks
-    items_editor.delete_all_items_from_trade
+    seller.delete_all_items_from_trade
 
-    items_editor.actualize_all_items
+    actualize_orders_and_items
   end
 
   private
 
-  def actualize_not_actualized_items
+  def actualize_not_actualized_orders
     order.actualize_orders if Order.where(status: NOT_ACTUALIZED_ORDER_STATUS).exist?
   end
 
