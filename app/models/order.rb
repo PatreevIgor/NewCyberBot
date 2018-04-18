8b1769553307848f5593353944a409293a21b56f
Patreev Igor <Patreewigor@yandex.ru>
2018-04-18 15:07:59 +0300

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,17 +1,17 @@
 class Order < ApplicationRecord
-  def create_order(order_hash)
-    Item.create(class_id:    order_hash[:class_id],
-                instance_id: order_hash[:instance_id],
-                hash_name:   order_hash[:hash_name],
-                price:       order_hash[:current_price],
-                link:        link_generator.generate_link(order_hash),
+  def create_order(item_hash)
+    Item.create(class_id:    item_hash[:class_id],
+                instance_id: item_hash[:instance_id],
+                hash_name:   item_hash[:hash_name],
+                price:       item_hash[:current_price],
+                link:        link_generator.generate_link(item_hash),
                 status:      Constant::NEW_ITEMS_STATUS)
   end
 
   def actualize_orders
     non_actualized_orders = Order.where(status: [NOT_ACTUALIZED_ORDER_STATUS, UNPROFITABLE_ORDER_STATUS])
     non_actualized_orders.each do |order|
-      if validator.profitable?(order_info_hash(order))
+      if item_validator.item_profitable?(order_info_hash(order))
         order.status = PROFITABLE_ORDER_STATUS
       else
         order.status = UNPROFITABLE_ORDER_STATUS
@@ -21,8 +21,8 @@ class Order < ApplicationRecord
 
   private
 
-  def validator
-    @validator ||= Validator.new
+  def item_validator
+    @item_validator ||= ItemValidator.new
   end
 
   def order_info_hash(order)
