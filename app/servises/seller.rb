9693e80a032ae708a93accbe77df4f36f426e616
Patreev Igor <Patreewigor@yandex.ru>
2018-04-18 15:42:19 +0300

    Update seller.rb

--- app/servises/seller.rb
+++ app/servises/seller.rb
@@ -5,7 +5,7 @@ class Seller
     profitable_orders.each do |order|
       Connection.send_request(format (Constant::CREATE_ORDER_URL, class_id:        item[class_id],
                                                                   instance_id:     item[instance_id],
-                                                                  price:           item[price],          
+                                                                  price:           price............,          
                                                                   your_secret_key: Rails.application.secrets.your_secret_key))
     end
     
@@ -23,5 +23,9 @@ class Seller
     
   def users_informator
     @users_informator ||= UserInformator.new
+  end
+  
+  def price
+    @price ||= Price.new
   end
 end
