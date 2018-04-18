c190392575c84a294f49b8f54d94bf7f73c37097
Patreev Igor <Patreewigor@yandex.ru>
2018-04-18 16:51:43 +0300

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,11 @@
 class Order < ApplicationRecord
-  def save_order(item_hash)
-    Item.create(class_id:    item_hash[:class_id],
-                instance_id: item_hash[:instance_id],
-                hash_name:   item_hash[:hash_name],
-                price:       price.price_of_buy_for_order(item_hash),
-                link:        link_generator.generate_link(item_hash),
-                status:      Constant::NEW_ITEMS_STATUS)
+  def create_order(item_hash)
+    Order.create(class_id:    item_hash[:class_id],
+                 instance_id: item_hash[:instance_id],
+                 hash_name:   item_hash[:hash_name],
+                 price:       price.price_of_buy_for_order(item_hash),
+                 link:        link_generator.generate_link(item_hash),
+                 status:      Constant::NEW_ITEMS_STATUS)
   end
 
   def actualize_orders
