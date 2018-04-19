87bef44c7de2526e6b93f0e58d8ac455dde980be
Patreev Igor <Patreewigor@yandex.ru>
2018-04-19 09:50:36 +0300

    Update item_finder.rb

--- app/servises/item_finder.rb
+++ app/servises/item_finder.rb
@@ -3,27 +3,27 @@
 class ItemFinder
   def find_actuall_items
     last_50_sales.each do |item_hash|
-      order.create_order(item_hash) if all_conditions_fulfilled?
+      seller.create_order(item_hash) if all_conditions_fulfilled?
     end
   end
 
   private
-
-  def all_conditions_fulfilled?
-    return true if item_validator.item_profitable?(item_hash) && order.order_not_exists?(item_hash)
-                                                              && item_validator.price_interval_is_valid?(item_hash)
-  end
   
   def last_50_sales
     Connection.send_request(Constant::LAST_50_SALES_URL)
+  end
+  
+  def all_conditions_fulfilled?
+    return true if item_validator.item_profitable?(item_hash) && order.order_not_exists?(item_hash)
+                                                              && item_validator.price_interval_is_valid?(item_hash)
   end
 
   def item_validator
     @item_validator ||= ItemValidator.new
   end
 
-  def item
-    @item ||= Item.new
+  def order
+    @order ||= Order.new
   end
 
   def seller
